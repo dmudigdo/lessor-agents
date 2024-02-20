c6b83d007f6bd6bdfa7e129c4d6f0911ba0ee828
Didi Mudigdo <dmudigdo@users.noreply.github.com>
2024-02-21 07:35:31 +0800

    Create re_headers.py

---
+++ re_headers.py
@@ -0,0 +1,43 @@
+# Put cookies and headers here, so the website does't detect you're a bot
+# WARNING: these expire quickly, so the below is a sample only. Get a fresh
+# one before you run, as per explanatory blog article.
+
+cookies = {
+    'reauid': '8d9654b8e05a01004e8d8f65ba0100001ef11b00',
+    'utag_main': 'v_id:018cb8c010210010106e1a52a2670504600350090086e$_sn:5$_se:9$_ss:0$_st:1707653716784$vapi_domain:realestate.com.au$dc_visit:5$ses_id:1707650224141%3Bexp-session$_pn:2%3Bexp-session$_prevpage:rea%3Arent%3Aproperty%20details%3Bexp-1707655516815$dc_event:8%3Bexp-session$dc_region:ap-southeast-2%3Bexp-session',
+    'split_audience': 'b',
+    'AMCV_341225BE55BBF7E17F000101%40AdobeOrg': '-330454231%7CMCIDTS%7C19765%7CMCMID%7C47234961578617247253515664118391607453%7CMCAID%7CNONE%7CMCOPTOUT-1707657427s%7CNONE%7CMCAAMLH-1708255027%7C8%7CMCAAMB-1708255027%7Cj8Odv6LonN4r3an7LhD3WZrU1bUpAkFkkiY1ncBR96t2PTI%7CMCSYNCSOP%7C411-19769%7CvVersion%7C3.1.2',
+    '_sp_id.2fe7': 'fa9799ca-3e69-4a1f-9dcb-8147467939fd.1703906648.5.1707651917.1707618055.248c19c9-9c80-47e6-9294-ff256319a0db',
+    'VT_LANG': 'language%3Den-US',
+    'mid': '15933649636612828640',
+    '_gcl_au': '1.1.1235941294.1703906653',
+    '_ga_F962Q8PWJ0': 'GS1.1.1707650231.5.1.1707651922.0.0.0',
+    '_ga': 'GA1.3.753389878.1703906653',
+    '_ga_3J0XCBB972': 'GS1.1.1707650231.5.1.1707651922.0.0.0',
+    's_ecid': 'MCMID%7C47234961578617247253515664118391607453',
+    '_fbp': 'fb.2.1703906657177.1959988297',
+    'nol_fpid': '40agazoxd8fc98bzorshldugtgpvx1703906662|1703906662102|1707651707372|1707651707551',
+    's_nr30': '1707651916415-Repeat',
+    'topid': 'REAUID:8D9654B8E05A01004E8D8F65BA0100001EF11B00',
+    'ab.storage.sessionId.746d0d98-0c96-45e9-82e3-9dfa6ee28794': '%7B%22g%22%3A%220684c09a-d8a6-61e9-a3ef-79429c3be843%22%2C%22e%22%3A1707653716859%2C%22c%22%3A1707650229038%2C%22l%22%3A1707651916859%7D',
+    'ab.storage.deviceId.746d0d98-0c96-45e9-82e3-9dfa6ee28794': '%7B%22g%22%3A%2257ef750e-96ce-f766-cbf4-ab7002916b3e%22%2C%22c%22%3A1707390997585%2C%22l%22%3A1707650229038%7D',
+    'KP_UIDz-ssn': '01X9uGjpLo8cRErxb62BPPONygPBMGY3Fg3Tav0Zicc2KMjzYKAJ7dOjkYTQJr86oCAlB7PPLjWp0LNLSJuiEclt2XoB37u4h2SWQljVZSHODF7RlcHjotlyjFPNKUaRXLfpIXHxfcpaOQjqMqcA8yyhGgX9Db',
+    'KP_UIDz': '01X9uGjpLo8cRErxb62BPPONygPBMGY3Fg3Tav0Zicc2KMjzYKAJ7dOjkYTQJr86oCAlB7PPLjWp0LNLSJuiEclt2XoB37u4h2SWQljVZSHODF7RlcHjotlyjFPNKUaRXLfpIXHxfcpaOQjqMqcA8yyhGgX9Db',
+    'Country': 'AU',
+    'KP_REF': '',
+}
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/119.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    # 'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+    # 'Cookie': 'reauid=8d9654b8e05a01004e8d8f65ba0100001ef11b00; utag_main=v_id:018cb8c010210010106e1a52a2670504600350090086e$_sn:5$_se:9$_ss:0$_st:1707653716784$vapi_domain:realestate.com.au$dc_visit:5$ses_id:1707650224141%3Bexp-session$_pn:2%3Bexp-session$_prevpage:rea%3Arent%3Aproperty%20details%3Bexp-1707655516815$dc_event:8%3Bexp-session$dc_region:ap-southeast-2%3Bexp-session; split_audience=b; AMCV_341225BE55BBF7E17F000101%40AdobeOrg=-330454231%7CMCIDTS%7C19765%7CMCMID%7C47234961578617247253515664118391607453%7CMCAID%7CNONE%7CMCOPTOUT-1707657427s%7CNONE%7CMCAAMLH-1708255027%7C8%7CMCAAMB-1708255027%7Cj8Odv6LonN4r3an7LhD3WZrU1bUpAkFkkiY1ncBR96t2PTI%7CMCSYNCSOP%7C411-19769%7CvVersion%7C3.1.2; _sp_id.2fe7=fa9799ca-3e69-4a1f-9dcb-8147467939fd.1703906648.5.1707651917.1707618055.248c19c9-9c80-47e6-9294-ff256319a0db; VT_LANG=language%3Den-US; mid=15933649636612828640; _gcl_au=1.1.1235941294.1703906653; _ga_F962Q8PWJ0=GS1.1.1707650231.5.1.1707651922.0.0.0; _ga=GA1.3.753389878.1703906653; _ga_3J0XCBB972=GS1.1.1707650231.5.1.1707651922.0.0.0; s_ecid=MCMID%7C47234961578617247253515664118391607453; _fbp=fb.2.1703906657177.1959988297; nol_fpid=40agazoxd8fc98bzorshldugtgpvx1703906662|1703906662102|1707651707372|1707651707551; s_nr30=1707651916415-Repeat; topid=REAUID:8D9654B8E05A01004E8D8F65BA0100001EF11B00; ab.storage.sessionId.746d0d98-0c96-45e9-82e3-9dfa6ee28794=%7B%22g%22%3A%220684c09a-d8a6-61e9-a3ef-79429c3be843%22%2C%22e%22%3A1707653716859%2C%22c%22%3A1707650229038%2C%22l%22%3A1707651916859%7D; ab.storage.deviceId.746d0d98-0c96-45e9-82e3-9dfa6ee28794=%7B%22g%22%3A%2257ef750e-96ce-f766-cbf4-ab7002916b3e%22%2C%22c%22%3A1707390997585%2C%22l%22%3A1707650229038%7D; KP_UIDz-ssn=01X9uGjpLo8cRErxb62BPPONygPBMGY3Fg3Tav0Zicc2KMjzYKAJ7dOjkYTQJr86oCAlB7PPLjWp0LNLSJuiEclt2XoB37u4h2SWQljVZSHODF7RlcHjotlyjFPNKUaRXLfpIXHxfcpaOQjqMqcA8yyhGgX9Db; KP_UIDz=01X9uGjpLo8cRErxb62BPPONygPBMGY3Fg3Tav0Zicc2KMjzYKAJ7dOjkYTQJr86oCAlB7PPLjWp0LNLSJuiEclt2XoB37u4h2SWQljVZSHODF7RlcHjotlyjFPNKUaRXLfpIXHxfcpaOQjqMqcA8yyhGgX9Db; Country=AU; KP_REF=',
+    'Upgrade-Insecure-Requests': '1',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'cross-site',
+    'Pragma': 'no-cache',
+    'Cache-Control': 'no-cache',
+}
